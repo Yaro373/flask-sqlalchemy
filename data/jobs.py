fa2573e1a5b52a0251c9e75c034e225ec7d1cf98
Yaro373 <zakoyasse@gmail.com>
2022-03-07 14:06:18 +0300

    initial commit

---
+++ data/jobs.py
@@ -0,0 +1,16 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+from sqlalchemy import orm
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
+    chief = sqlalchemy.Column(sqlalchemy.Integer)
+    members = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String)
+    orm.relation('User')
+
---
+++ main.py
@@ -0,0 +1,100 @@
+from flask import Flask
+from flask import render_template, redirect
+from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from data.news import News
+from data.user import RegisterForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    news = db_sess.query(News).filter(News.is_private != True)
+    return render_template("index.html", news=news)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+def main():
+    db_session.global_init("db/blogs.db")
+
+    user = User()
+    user.surname = "Scott"
+    user.name = "Ridley"
+    user.age = 21
+    user.position = "captain"
+    user.speciality = "research engineer"
+    user.address = "module_1"
+    user.email = "scott_chief@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Bob"
+    user.name = "Bidly"
+    user.age = 20
+    user.position = "worker"
+    user.speciality = "engineer"
+    user.address = "module_1"
+    user.email = "bob_bidly@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Ben"
+    user.name = "Bitly"
+    user.age = 20
+    user.position = "worker"
+    user.speciality = "engineer"
+    user.address = "module_1"
+    user.email = "ben_bitly@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    user = User()
+    user.surname = "Mark"
+    user.name = "Shetly"
+    user.age = 19
+    user.position = "worker"
+    user.speciality = "engineer"
+    user.address = "module_1"
+    user.email = "mark_shetly@mars.org"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
